f760a6e30762d4fe0ac0cf60b7a2ada413a0ddb3
tatiana.mojica <tatiana.mojica@bustle.com>
2019-08-24 18:46:24 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your code in this file!
+function scuberGreetingForFeet(someValue) {
+  if (someValue <= 400) {
+    return 'This one is on me!';
+  } else if (someValue > 2000 && someValue < 2500) {
+    return 'I will gladly take your thirty bucks.';
+  } else if (someValue > 2500) {
+    return 'No can do.';
+  }
+}
+
+
+function ternaryCheckCity(aCity){
+  if (aCity === 'NYC') {
+    return 'Ok, sounds good.'
+  } else if (aCity != 'NYC') {
+      return 'No go.'
+  }
+}
+
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+      break;
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+  }
+}
